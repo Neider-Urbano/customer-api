2d19616dc2841dcf9439bfcb16a2a7daafb5ca8e
neider urbano <jjuly8julianur@gmail.com>
2024-07-04 08:19:12 -0500

    adjust inicializing data base

--- src/main/java/com/example/customer_api/CustomerApiApplication.java
+++ src/main/java/com/example/customer_api/CustomerApiApplication.java
@@ -19,7 +19,12 @@ public class CustomerApiApplication {
 	@PostConstruct
 	public void init() {
 		System.out.println("Initializing database...");
-		jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS customer_db.customer (id BIGINT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50) NOT NULL, email VARCHAR(100) NOT NULL UNIQUE, phone_number VARCHAR(15));");
-		jdbcTemplate.execute("INSERT INTO customer_db.customer (name, email, phone_number) VALUES ('Neider Urbano', 'neider@example.com', '3204524545'), ('Julian Bastilla', 'julian@example.com', '3202343800');");
+		String sql = "INSERT INTO customer_db.customer (name, email, phone_number) " +
+				"SELECT * FROM (SELECT 'Neider Urbano' AS name, 'neider@example.com' AS email, '3204524545' AS phone_number " +
+				"UNION ALL " +
+				"SELECT 'Julian Bastilla', 'julian@example.com', '3202343800') AS new_customers " +
+				"WHERE NOT EXISTS (SELECT 1 FROM customer WHERE customer.email = new_customers.email)";
+
+		jdbcTemplate.execute(sql);
 	}
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,9 @@
-INSERT INTO customer_db.customer (name, email, phone_number) VALUES ('Neider Urbano', 'neider@example.com', '3204524545'), ('Julian Bastilla', 'julian@example.com', '3202343800')
+INSERT INTO customer_db.customer (name, email, phone_number)
+SELECT * FROM (
+    SELECT 'Neider Urbano' AS name, 'neider@example.com' AS email, '3204524545' AS phone_number
+    UNION ALL
+    SELECT 'Julian Bastilla', 'julian@example.com', '3202343800'
+) AS new_customers
+WHERE NOT EXISTS (
+    SELECT 1 FROM customer WHERE customer.email = new_customers.email
+);
